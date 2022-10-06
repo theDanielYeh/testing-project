b8ee524eace74480701dfa3fe5de72d077dc5ede
Daniel Yeh <dyeh1989@gmail.com>
2022-10-05 22:31:53 -0700

    completed test 3, 4

--- cypress/e2e/integration/sold-tests/general.cy.js
+++ cypress/e2e/integration/sold-tests/general.cy.js
@@ -20,5 +20,21 @@ describe('general.spec.js', () => {
   })
 
   // Test 3
+  it('Add todo, should clear text input field when an item is added, assert field is blank', () => {
+    cy.get('input.new-todo')
+      .type('todo1{enter}')
+      .should('not.have.value')
+  })
 
+  // Test 4
+  it('Add todo, assert the main and footer sections should not be hidden', () => {
+    cy.get('input.new-todo')
+      .type('todo1{enter}')
+
+    cy.get('.main')
+      .should('be.visible');
+
+    cy.get('.footer')
+      .should('be.visible');
+  })
 })
