83dd771203ab6477677b5e6f92a40451a2ad5be3
Daniel Yeh <dyeh1989@gmail.com>
2022-10-05 23:05:48 -0700

    completed general test 5

--- cypress/e2e/integration/sold-tests/general.cy.js
+++ cypress/e2e/integration/sold-tests/general.cy.js
@@ -6,11 +6,17 @@ describe('general.spec.js', () => {
 
   // Test 1
   it('Initial page display, assert the main and footer sections should be hidden', () => {
-    cy.get('main')
-      .should('not.be.visible');
+    // cy.get('.main')
+    //   .should('not.be.visible');
 
-    cy.get('footer')
-      .should('not.be.visible');
+    // cy.get('.footer')
+    //   .should('not.be.visible');
+
+    cy.get('.main')
+      .should('not.exist');
+
+    cy.get('.footer')
+      .should('not.exist');
   })
 
   // Test 2
@@ -21,20 +27,36 @@ describe('general.spec.js', () => {
 
   // Test 3
   it('Add todo, should clear text input field when an item is added, assert field is blank', () => {
+    const newItem = 'Todo1';
+
     cy.get('input.new-todo')
-      .type('todo1{enter}')
+      .type(`${newItem}{enter}`)
       .should('not.have.value')
   })
 
   // Test 4
   it('Add todo, assert the main and footer sections should not be hidden', () => {
+    const newItem = 'Todo1';
+
     cy.get('input.new-todo')
-      .type('todo1{enter}')
+      .type(`${newItem}{enter}`)
 
     cy.get('.main')
       .should('be.visible');
 
     cy.get('.footer')
       .should('be.visible');
+  })
+
+  // Test 5
+  it('Add new todo item and assert that it exists', () => {
+    const newItem = 'Todo1';
+
+    cy.get('input.new-todo')
+      .type(`${newItem}{enter}`)
+
+    cy.get('.todo-list > li')
+      .last()
+      .should('have.text', newItem)
   })
 })
